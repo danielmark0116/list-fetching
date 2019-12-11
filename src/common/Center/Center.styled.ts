8ae363fbc7966e15c759c3f4da81b1b87270779a
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-12-11 13:08:21 +0100

    Add wrap for center component

--- src/common/Center/Center.styled.ts
+++ src/common/Center/Center.styled.ts
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
 export const Center = styled.div`
+  width: auto;
   display: flex;
   justify-content: center;
   text-align: center;
+  flex-wrap: wrap;
 `;
